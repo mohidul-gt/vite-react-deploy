c2882c234633fc1573a0f91828d5d6a44880af77
mohidul-gt <mohidul.islam@gigatechltd.com>
2024-10-30 16:01:20 +0600

    basic design

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <>      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,9 +19,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>No Copyright.</p>
+      <p style={{ color: "#888", fontSize: "12px", textAlign: "center" }}>No Copyright.</p>
     </>
   )
+  
 }
 
 export default App
